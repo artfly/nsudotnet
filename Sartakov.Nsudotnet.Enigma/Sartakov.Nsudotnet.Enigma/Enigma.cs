0a88b61049009f08eca86943dd2e4bb733e4bb11
artfly <artfly94@gmail.com>
2016-05-15 01:17:59 +0600

    Add Enigma

---
+++ Sartakov.Nsudotnet.Enigma/Sartakov.Nsudotnet.Enigma/Enigma.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Sartakov.Nsudotnet.Enigma
+{
+  public class Enigma
+  {
+
+    public static void Encrypt(string inName, SymmetricAlgorithm algorithm, string outName)
+    {
+      try
+      {
+        using (var fin = new FileStream(inName, FileMode.Open, FileAccess.Read))
+        {
+          var fout = new FileStream(outName, FileMode.Create, FileAccess.Write);
+          using (var cryptoStream = new CryptoStream(fout, algorithm.CreateEncryptor(), CryptoStreamMode.Write))
+          {
+            CopyStream(fin, cryptoStream);
+          }
+        }
+      }
+      catch (IOException e)
+      {
+        Console.WriteLine(e.Message);
+        return;
+      }
+
+      string[] base64Repr = {Convert.ToBase64String(algorithm.IV), Convert.ToBase64String(algorithm.Key)};
+      var keyName = inName.EndsWith(".txt") ? inName.Insert(outName.Length - 4, ".key") : string.Format("{0}.key.txt", inName);
+      Console.WriteLine(keyName);
+      using (var keyFile = new StreamWriter(keyName))
+      {
+        keyFile.WriteLine(base64Repr[0]);
+        keyFile.WriteLine(base64Repr[1]);
+      }
+      Console.WriteLine("Encrypted!");
+    }
+
+    public static void Decrypt(string inName, SymmetricAlgorithm algorithm, string outName, string keyName)
+    {
+      try
+      {
+        using (var keyFile = new StreamReader(keyName))
+        {
+          algorithm.IV = Convert.FromBase64String(keyFile.ReadLine());
+          algorithm.Key = Convert.FromBase64String(keyFile.ReadLine());
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return;
+      }
+
+      try
+      {
+        using (var fin = new FileStream(inName, FileMode.Open, FileAccess.Read))
+        {
+          var fout = new FileStream(outName, FileMode.Create, FileAccess.Write);
+          using (var cryptoStream = new CryptoStream(fout, algorithm.CreateDecryptor(), CryptoStreamMode.Write))
+          {
+            CopyStream(fin, cryptoStream);
+          }
+        }
+      }
+      catch (IOException e)
+      {
+        Console.WriteLine(e.Message);
+        return;
+      }
+      Console.WriteLine("Decrypted!");
+    }
+
+      private static void CopyStream(Stream fin, Stream fout)
+      {
+          var buffer = new byte[256];
+          long written = 0;
+          var total = fin.Length;
+          while (written < total)
+          {
+            var curlen = fin.Read(buffer, 0, 256);
+            fout.Write(buffer, 0, curlen);
+            written += curlen;
+          }
+      }
+  }
+}
---
+++ Sartakov.Nsudotnet.Enigma/Sartakov.Nsudotnet.Enigma/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Sartakov.Nsudotnet.Enigma
+{
+  class Program
+  {
+    private static void ShowHelp()
+    {
+      Console.WriteLine(
+        @"Usage :
+		{0} encrypt FILE ALGORITHM OUTPUT_FILE
+		{0} decrypt FILE ALGORITHM KEY_FILE OUTPUT_FILE", System.AppDomain.CurrentDomain.FriendlyName
+      );
+    }
+
+    static void Main(string[] args)
+    {
+      if (args.Length == 0)
+      {
+        ShowHelp();
+        return;
+      }
+      if ((args.Length != 4 || args[0] != "encrypt") && (args.Length != 5 || args[0] != "decrypt"))
+      {
+        ShowHelp();
+        return;
+      }
+
+      SymmetricAlgorithm algorithm;
+      switch (args[2])
+      {
+        case "aes":
+          algorithm = new AesCryptoServiceProvider();
+          break;
+        case "des":
+          algorithm = new DESCryptoServiceProvider();
+          break;
+        case "rc2":
+          algorithm = new RC2CryptoServiceProvider();
+          break;
+        case "rijndael":
+          algorithm = new RijndaelManaged();
+          break;
+        default:
+          ShowHelp();
+          return;
+      }
+
+      switch (args[0])
+      {
+        case "encrypt":
+          Enigma.Encrypt(args[1], algorithm, args[3]);
+          break;
+        case "decrypt":
+          Enigma.Decrypt(args[1], algorithm, args[4], args[3]);
+          break;
+      }
+    }
+  }
+}
+
+
