f5ae7b0e5f3d636f41ef80f6f55d56aa54f06bb0
artfly <artfly94@gmail.com>
2016-05-23 18:53:49 +0600

    Refactor for string input

--- Sartakov.Nsudotnet.LinesCounter/Sartakov.Nsudotnet.LinesCounter/Program.cs
+++ Sartakov.Nsudotnet.LinesCounter/Sartakov.Nsudotnet.LinesCounter/Program.cs
@@ -3,91 +3,82 @@ using System.IO;
 
 namespace Sartakov.Nsudotnet.LinesCounter
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      if (args.Length != 1)
-      {
-        Console.WriteLine("Usage : {0} FORMAT");
-        return;
-      }
-      CountLines(args[0]);
-    }
-
-    private static void CountLines(string format)
-    {
-      var counter = 0;
-      int c;
-      char prev;
-      bool charsAppeared;
-      bool singleLineComment;
-      bool multipleLinesComment;
-
-      foreach (var fileName in Directory.EnumerateFiles(".", format, SearchOption.AllDirectories))
-      {
-        using (var sr = new StreamReader(fileName))
+        static void Main(string[] args)
         {
-          prev = '\n';
-          charsAppeared = false;
-          singleLineComment = false;
-          multipleLinesComment = false;
-          while ((c = sr.Read()) > 0)
-          {
-            switch ((char) c)
+            if (args.Length != 1)
             {
-              case '/':
-                if (prev == '/' && !singleLineComment && !multipleLinesComment)
-                {
-                  if (charsAppeared)
-                  {
-                    counter++;
-                    charsAppeared = false;
-                  }
-                  singleLineComment = true;
-                }
-                else if (prev == '*' && !singleLineComment && multipleLinesComment)
-                {
-                    multipleLinesComment = false;
-                }
-                break;
-              case '*':
-                if (prev == '/' && !multipleLinesComment && !singleLineComment)
-                {
-                  if (charsAppeared)
-                  {
-                    counter++;
-                    charsAppeared = false;
-                  }
-                  multipleLinesComment = true;
-                }
-                break;
-              case '\n':
-                if (charsAppeared && !singleLineComment && !multipleLinesComment)
-                {
-                  counter++;
-                }
-                singleLineComment = false;
-                charsAppeared = false;
-                break;
-              default:
-                if (!multipleLinesComment && !singleLineComment && (char)c != ' ' && (char)c != '\t')
-                {
-                  charsAppeared = true;
-                }
-                break;
-          	}
-            prev = (char) c;
-          }
-          if (charsAppeared && !singleLineComment && !multipleLinesComment)
-          {
-            counter++;
-          }
+                Console.WriteLine("Usage : {0} FORMAT");
+                return;
+            }
+            CountLines(args[0]);
         }
-      }
-      Console.WriteLine("Total lines count : {0}", counter);
+
+        private static void CountLines(string format)
+        {
+            var counter = 0;
+            bool inComment = false;
+            bool hasCode = false;
+            string line;
+
+            foreach (var fileName in Directory.EnumerateFiles(".", format, SearchOption.AllDirectories))
+            {
+                using (var sr = new StreamReader(fileName))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        hasCode = false;
+                        line = line.Trim();
+                        if (String.IsNullOrEmpty(line))
+                        {
+                            continue;
+                        }
+                        for (int i = 1; i < line.Length; i++)
+                        {
+                            if (!inComment)
+                            {
+
+                                if (line[i - 1] == '/' && line[i] == '/')
+                                {
+                                    if (i != 1)
+                                    {
+                                        hasCode = true;
+                                    }
+                                    break;
+                                }
+                                else if (line[i - 1] == '/' && line[i] == '*')
+                                {
+                                    inComment = true;
+                                    if (i != 1)
+                                    {
+                                        hasCode = true;
+                                    }
+                                    i++;
+                                }
+                                else if (line[i] != ' ')
+                                {
+                                    hasCode = true;
+                                }
+                            }
+                            else
+                            {
+                                if (line[i - 1] == '*' && line[i] == '/')
+                                {
+                                    inComment = false;
+                                    i++;
+                                }
+                            }
+                        }
+
+                        if (hasCode || line.Length == 1 && line[0] != ' ')
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Total lines count : {0}", counter);
+        }
     }
-  }
 }
-
-
